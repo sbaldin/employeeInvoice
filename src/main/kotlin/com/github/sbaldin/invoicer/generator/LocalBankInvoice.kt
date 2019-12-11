3fa5ce8094cf7c5a4a4e8c5d1eb7418083a6d949
Sergey Baldin <sbaldin@riskmatch.com>
2019-12-11 14:35:35 +0700

    formatted tables in local bank

--- src/main/kotlin/com/github/sbaldin/invoicer/generator/LocalBankInvoice.kt
+++ src/main/kotlin/com/github/sbaldin/invoicer/generator/LocalBankInvoice.kt
@@ -12,41 +12,40 @@ import java.util.*
 
 
 class LocalBankInvoice(
-   private val appConf: AppConf,
-   private val employee: EmployeeDetails,
-   private val localBankingDetails: LocalBankingDetails,
-   private val foreignBankingDetails: ForeignBankingDetails
-    ) : InvoiceGenerator {
+    private val appConf: AppConf,
+    private val employee: EmployeeDetails,
+    private val localBankingDetails: LocalBankingDetails,
+    private val foreignBankingDetails: ForeignBankingDetails
+) : InvoiceGenerator {
 
     override fun generate() {
         val document = XWPFDocument()
-
         document.apply {
-            val paragraph = createParagraph()
-            val run = paragraph.createRun()
-            run.setText("Инвойс № ${employee.invoiceNumber()} от ${employee.formattedInvoiceDate(Locale("ru"))}")
-            run.fontFamily = "Roboto"
-            run.isBold = true
-            paragraph.style = "Heading1"
-            paragraph.alignment = ParagraphAlignment.CENTER
-            setSingleLineSpacing(paragraph)
+            singleLineParagraph("Инвойс № ${employee.invoiceNumber()} от ${employee.formattedInvoiceDate(Locale("ru"))}") {
+                it.style = "Heading1"
+                it.alignment = ParagraphAlignment.CENTER
+                setSingleLineSpacing(it)
 
+                val run = it.runs.first()
+                run.fontFamily = "Roboto"
+                run.isBold = true
+            }
             createMonthRateTable()
 
-            createParagraph()
-            val createParagraph = createParagraph()
-            createParagraph.createRun().setText("Оплатить в срок до ${employee.formattedPaymentDeadline(Locale("ru"))}.")
-            createParagraph()
-            createParagraph().createRun().setText("Выставлен:")
+            singleLineParagraph("Оплатить в срок до ${employee.formattedPaymentDeadline(Locale("ru"))}.")
 
             createForeignBankDetailsTable()
             createLocalBankDetails()
 
-            createParagraph()
-            createParagraph().createRun().setText("Подпись")
+            singleLineParagraph("Подпись ") {
+                val run = it.runs.first()
+                run.fontFamily = "Roboto"
+                val underLinePart = it.createRun()
+                underLinePart.underline = UnderlinePatterns.SINGLE
+                underLinePart.setText("             ")
+                setDoublerLineSpacing(it)
+            }
         }
-        //create Paragraph
-
         File(appConf.outputPath + "/invoice_test.docx").outputStream().apply {
             document.write(this)
             close()
@@ -73,96 +72,86 @@ class LocalBankInvoice(
         createParagraph().body.insertTable(0, monthRateTable)
     }
 
-    private fun XWPFDocument.createForeignBankDetailsTable(){
-        val foreignBankDetailsTable = createTable().apply {
+    private fun XWPFDocument.createForeignBankDetailsTable() {
+        val titleParagraph = createParagraph()
+        val titleRun = titleParagraph.createRun()
+        titleRun.fontFamily = "Roboto"
+        titleRun.setText("Выставлен:")
+        setDoublerLineSpacing(titleParagraph)
+        val foreignBankDetailsTable = createTable(4, 2).apply {
             //create first row
             //create first row
             val bankName: XWPFTableRow = getRow(0)
-            bankName.getCell(0).text = "Наименование Банка"
-            bankName.addNewTableCell().text = foreignBankingDetails.name
 
-            val accountNumber: XWPFTableRow = createRow()
-            accountNumber.getCell(0).text = "Номер Счета"
-            accountNumber.getCell(1).text = foreignBankingDetails.accountNumber
+            setTextWithDefaultStyle(bankName.getCell(0), "Наименование Банка")
+            setTextWithDefaultStyle(bankName.getCell(1), foreignBankingDetails.name)
 
-            val employeeName: XWPFTableRow = createRow()
-            employeeName.getCell(0).text = "Имя Отправителя"
-            employeeName.getCell(1).text = foreignBankingDetails.contractorName
+            val accountNumber: XWPFTableRow = getRow(1)
+            setTextWithDefaultStyle(accountNumber.getCell(0), "Номер Счета")
+            setTextWithDefaultStyle(accountNumber.getCell(1), foreignBankingDetails.accountNumber)
 
-            val address: XWPFTableRow = createRow()
-            address.getCell(0).text = "Адрес"
-            address.getCell(1).text = foreignBankingDetails.address
+            val employeeName: XWPFTableRow = getRow(2)
+            setTextWithDefaultStyle(employeeName.getCell(0), "Имя Отправителя")
+            setTextWithDefaultStyle(employeeName.getCell(1), foreignBankingDetails.contractorName)
+
+            val address: XWPFTableRow = getRow(3)
+            setTextWithDefaultStyle(address.getCell(0), "Адрес")
+            setTextWithDefaultStyle(address.getCell(1), foreignBankingDetails.address)
         }
         createParagraph().body.insertTable(0, foreignBankDetailsTable)
-
     }
-    
-    private fun XWPFDocument.createLocalBankDetails() {
-        createParagraph()
-        createParagraph().createRun().setText("Получатель:")
 
-        val localBankDetailsTable = createTable().apply {
+    private fun XWPFDocument.createLocalBankDetails() {
+        val titleParagraph = createParagraph()
+        val titleRun = titleParagraph.createRun()
+        titleRun.fontFamily = "Roboto"
+        titleRun.setText("Получатель:")
+        setDoublerLineSpacing(titleParagraph)
+        val localBankDetailsTable = createTable(4, 2).apply {
             //create first row
             //create first row
             val bankName: XWPFTableRow = getRow(0)
-            bankName.getCell(0).text = "Наименование Банка"
-            bankName.addNewTableCell().text = localBankingDetails.name
+            setTextWithDefaultStyle(bankName.getCell(0), "Наименование Банка")
+            setTextWithDefaultStyle(bankName.getCell(1), localBankingDetails.name)
 
-            val accountNumber: XWPFTableRow = createRow()
-            accountNumber.getCell(0).text = "Номер Счета"
-            accountNumber.getCell(1).text = localBankingDetails.accountNumber
+            val accountNumber: XWPFTableRow = getRow(1)
+            setTextWithDefaultStyle(accountNumber.getCell(0), "Номер Счета")
+            setTextWithDefaultStyle(accountNumber.getCell(1), localBankingDetails.accountNumber)
 
-            val employeeName: XWPFTableRow = createRow()
-            employeeName.getCell(0).text = "Имя Получателя"
-            employeeName.getCell(1).text = localBankingDetails.beneficiaryName
+            val employeeName: XWPFTableRow = getRow(2)
+            setTextWithDefaultStyle(employeeName.getCell(0), "Имя Получателя")
+            setTextWithDefaultStyle(employeeName.getCell(1), localBankingDetails.beneficiaryName)
 
-            val address: XWPFTableRow = createRow()
-            address.getCell(0).text = "Адрес"
-            address.getCell(1).text = localBankingDetails.beneficiaryAddress
+            val address: XWPFTableRow = getRow(3)
+            setTextWithDefaultStyle(address.getCell(0), "Адрес")
+            setTextWithDefaultStyle(address.getCell(1), localBankingDetails.beneficiaryAddress)
         }
 
         createParagraph().body.insertTable(0, localBankDetailsTable)
     }
 
-
-    private fun XWPFDocument.addCustomHeadingStyle(
-        styleId: String,
-        headingLevel: Int
-    ) {
-        val ctStyle = CTStyle.Factory.newInstance()
-        ctStyle.styleId = styleId
-        val styleName = CTString.Factory.newInstance()
-        styleName.setVal(styleId)
-        ctStyle.name = styleName
-        val indentNumber = CTDecimalNumber.Factory.newInstance()
-        indentNumber.setVal(BigInteger.valueOf(headingLevel.toLong()))
-        // lower number > style is more prominent in the formats bar
-        ctStyle.uiPriority = indentNumber
-        val onoffnull = CTOnOff.Factory.newInstance()
-        ctStyle.unhideWhenUsed = onoffnull
-        // style shows up in the formats bar
-        ctStyle.qFormat = onoffnull
-        // style defines a heading of the given level
-        val ppr = CTPPr.Factory.newInstance()
-        ppr.outlineLvl = indentNumber
-        ctStyle.pPr = ppr
-        val style = XWPFStyle(ctStyle)
-        // is a null op if already defined
-        val styles = createStyles()
-        style.type = STStyleType.PARAGRAPH
-        styles.addStyle(style)
+    private fun XWPFDocument.singleLineParagraph(
+        text: String,
+        paragraphStyle: (XWPFParagraph) -> Unit = { setSingleLineSpacing(it) }
+    ): XWPFParagraph? {
+        val paragraph = createParagraph()
+        val run = paragraph.createRun()
+        run.fontFamily = "Roboto"
+        run.setText(text)
+        paragraphStyle(paragraph)
+        return paragraph
     }
-
-
 
     private fun setTextWithDefaultStyle(cell: XWPFTableCell, text: String) {
         cell.verticalAlignment = XWPFTableCell.XWPFVertAlign.CENTER
         //Can't get first paragraph with elegant way, looks like ugly code is major feature of Apache POI
         val ctP: CTP = if (cell.ctTc.sizeOfPArray() == 0) cell.ctTc.addNewP() else cell.ctTc.getPArray(0)
-        val par = XWPFParagraph(ctP, cell)
-        setSingleLineSpacing(par)
-        par.alignment = ParagraphAlignment.CENTER
-        val run = par.createRun()
+        val paragraph = XWPFParagraph(ctP, cell)
+        setSingleLineSpacing(paragraph)
+        paragraph.alignment = ParagraphAlignment.CENTER
+        paragraph.style = "Text Body"
+        val run = paragraph.createRun()
+        run.fontFamily = "Roboto"
         run.setText(text)
     }
 
@@ -172,6 +161,16 @@ class LocalBankInvoice(
         val spacing = if (ppr!!.isSetSpacing) ppr.spacing else ppr.addNewSpacing()
         spacing.after = BigInteger.valueOf(100)
         spacing.before = BigInteger.valueOf(100)
+        spacing.lineRule = STLineSpacingRule.AUTO
+        spacing.line = BigInteger.valueOf(240)
+    }
+
+    private fun setDoublerLineSpacing(paragraph: XWPFParagraph) {
+        var ppr = paragraph.ctp.pPr
+        if (ppr == null) ppr = paragraph.ctp.addNewPPr()
+        val spacing = if (ppr!!.isSetSpacing) ppr.spacing else ppr.addNewSpacing()
+        spacing.after = BigInteger.valueOf(200)
+        spacing.before = BigInteger.valueOf(200)
         spacing.lineRule = STLineSpacingRule.AUTO
         spacing.line = BigInteger.valueOf(240)
     }
